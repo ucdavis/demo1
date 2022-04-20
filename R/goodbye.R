b3131b34fb14707db20aefb10c6a3dda2351290c
Douglas Ezra Morrison <dezramorrison@gmail.com>
2022-04-20 14:47:48 -0700

    Initial commit

---
+++ R/goodbye.R
@@ -0,0 +1,15 @@
+#' Goodbye function
+#'
+#' This function prints "goodbye" when it is called.
+#'
+#' @param arg1 this argument does nothing
+#'
+#' @return nothing
+#' @export
+#'
+#' @examples
+#' goodbye("dummy arg")
+goodbye = function(arg1)
+{
+  print("Goodbye, for now.")
+}
